a8ff00377af1e30b2544446d5e013b334b95306b
foxneZz <foxnezz@gmail.com>
2016-11-03 22:31:44 +0300

    some refactoring / more output

--- photo-handler.rb
+++ photo-handler.rb
@@ -16,37 +16,40 @@ FILENAME_FORMAT_INCLUDE_INDEX = true
 
 EXTENSION_WORD = "Extension"
 FILE_COUNT_WORD = "Number"
-EXTENSION_SIZE_WORD = "Size"
+SIZE_WORD = "Size"
+MEDIA_TYPE_WORD = "Media type"
 TOTAL_WORD = "TOTAL"
 
 ALIGN_TABLE = [:center, :right, :right]
 
 fail "ERROR: The output directory already contains files" if Dir.new(OUTPUT_DIR).count > 2
 
-print "Scanning for files... "
+print "Scanning for files..."
 file_paths = Dir[File.join(INPUT_DIR, '**', '*')].reject { |path| File.directory? path }
-puts "Done"
+puts " Done"
 
 def extension_of path
     File.extname(path)[1..-1].downcase
 end
 
+FileWithStats = Struct.new :path, :stats
+
 files_by_ext = Hash.new 0
 files_by_size = Hash.new { |h, k| h[k] = [] }
 size_by_ext = Hash.new 0
 file_paths.each do |path|
-    size = File.size path
+    file = FileWithStats.new path, File.stat(path)
     ext = extension_of path
-    files_by_size[size] << path
+    files_by_size[file.stats.size] << file
     files_by_ext[ext] += 1
-    size_by_ext[ext] += size
+    size_by_ext[ext] += file.stats.size
 end
 
 def format_size size
     "%.02f MiB" % (size.to_f / (2**20))
 end
 
-table = Terminal::Table.new :headings => [EXTENSION_WORD, FILE_COUNT_WORD, EXTENSION_SIZE_WORD],
+table = Terminal::Table.new :headings => [EXTENSION_WORD, FILE_COUNT_WORD, SIZE_WORD],
     :rows => files_by_ext.map { |ext, count| [ext, count, format_size(size_by_ext[ext])] }
 table.add_separator
 table.add_row [TOTAL_WORD, files_by_ext.values.inject(:+), format_size(size_by_ext.values.inject(:+))]
@@ -55,41 +58,49 @@ puts table
 
 print "Discarding the duplicates..."
 unique_files = []
-files_by_size.each do |size, paths_i|
-    if paths_i.count == 1
-        unique_files << paths_i.first
+files_by_size.each do |size, files_i|
+    if files_i.count == 1
+        unique_files << files_i.first
         next
     end
     checksums = Hash.new { |h, k| h[k] = [] }
-    paths_i.each { |path| checksums[Digest::MD5.file(path).hexdigest] << path }
-    checksums.each do |checksum, paths_j|
-        if paths_j.count == 1
-            unique_files << paths_j.first
+    files_i.each { |file| checksums[Digest::MD5.file(file.path).hexdigest] << file }
+    checksums.each do |checksum, files_j|
+        if files_j.count == 1
+            unique_files << files_j.first
             next
         end
-        unique_files << paths_j.min_by { |path| File.stat(path).mtime }
+        unique_files << files_j.min_by { |file| file.stats.mtime }
     end
 end
 puts " Done"
 
-exit
+puts "#{file_paths.count - unique_files.count} duplicates discarded", "Unique file count: #{unique_files.count}"
 
-puts "all file count: #{file_paths.count}", "unique file count: #{unique_files.count}"
-
+print "Sorting into types..."
 files_by_type = Hash.new { |h, k| h[k] = [] }
-unique_files.each do |path|
-    extension = File.extname(path)[1..-1].downcase
+unique_files.each do |file|
+    extension = extension_of file.path
     MEDIA_EXTENSIONS.each do |type, extensions|
         if extensions.include? extension
-            files_by_type[type] << path
+            files_by_type[type] << file
             break
         end
     end
 end
+puts " Done"
+
+size_by_type = Hash[files_by_type.map { |type, files| [type, files.map { |file| file.stats.size }.inject(:+)] }]
+table = Terminal::Table.new :headings => [MEDIA_TYPE_WORD, FILE_COUNT_WORD, SIZE_WORD],
+    :rows => files_by_type.map { |type, files| [type, files.count, format_size(size_by_type[type])] }
+ALIGN_TABLE.each.with_index { |align, index| table.align_column index, align }
+puts table
+
+exit
 
 files_by_type.each { |type, paths| puts "#{type}: #{paths.count}" }
 
-Jpg = Struct.new(:path, :metadata, :time)
+Jpg = Struct.new :path, :metadata, :time
 
 photos_by_cam = Hash.new { |h, k| h[k] = [] }
 files_by_type[:photo].each.with_index do |path, index|
@@ -98,13 +109,9 @@ files_by_type[:photo].each.with_index do |path, index|
     photos_by_cam["#{jpg.metadata.make} #{jpg.metadata.model}".gsub(/[\W&&[^ ]]+/, "").scan(/\b\w+\b/).uniq.join " "] << jpg
 end
 
-#EXIFR::JPEG.new(files_by_type[:photo].first).to_hash.each { |key, value| puts "#{key}: #{value}" }
-
 count = photos_by_cam.map { |cam, photos| photos.count }.inject(:+)
 i = 0
 photos_by_cam.each do |cam, photos|
-#    puts "#{cam}: #{photos.count}"
-
     photos.sort_by! { |photo| photo.time }
 
     out_dir = File.join(OUTPUT_DIR, cam)
